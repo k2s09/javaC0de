9b27bbe75f7a10ae25900b21a5465e692bbb4209
Kavin Sood <68739182+k2s09@users.noreply.github.com>
2023-03-29 17:03:09 +0530

    adding file

--- spiralPrint.java
+++ spiralPrint.java
@@ -1,43 +1,39 @@
-import java.util.Scanner;
+// Spiral printing an NxN matrix
 public class spiralPrint {
+    static int N = 4;
     public static void main(String[] args) {
+
         // Inputs
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter N");
-        int N = sc.nextInt();
         int[][] arr = new int[N][N];
-        int c = 1;
-        for(int i = 0; i < N; i++) {
+        int c = 0;
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                arr[i][j] = c++;
+                arr[i][j] = ++c;
             }
         }
-        // Full Printing
-        for(int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+        printSpiral(arr);
+        // Spiral printing
+        // Top Line
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i][0] + " ");
         }
-        // Determine the number of layers to be printed, Even = N/2, Odd = (N+1)/2
-        // Spiral Print
-        int layers = (N % 2 == 0)? (N / 2) : ((N+1)/2);
-        for (int i = 0; i < layers; i++) {
-            // Top Line
-            for (int j = i; j < N-i-1; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            // Right Line
-            for (int j = i; j < N-i-1; j++) {
-                System.out.print(arr[j][N-i-1] + " ");
-            }
-            // Bottom Line
-            for (int j = N-i-1; j > i; j--) {
-                System.out.print(arr[N-i-1][j] + " ");
-            }
-            // Left Line
-            for (int j = N-i-1; j > i; j--) {
-                System.out.print(arr[j][i] + " ");
+        // Right Line
+        for (int i = 1; i < N; i++) {
+            System.out.print(arr[N-1][i] + " ");
+        }
+        // Bottom Line
+        for (int i = N-2; i >= 0; i--) {
+            System.out.print(arr[i][N-1]);
+        }
+        // Left Line
+        for(int i = N-2; i >= 1; i--) {
+            System.out.print(arr[0][i]);
+        }
+    }
+    static void printSpiral(int[][] arr) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j]);
             }
             System.out.println();
         }
